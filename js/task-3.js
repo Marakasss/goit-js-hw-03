7fd7254d34720046cb6e5134e322cb51e3decdfe
Marakas <igorpetriv.bsc@gmail.com>
2025-01-07 15:55:03 +0200

    done

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function filterArray(numbers, value) {
+    let numArray = [];
+    for (const number of numbers) {
+        if (number > value) {
+            numArray.push(number);
+        }
+    }
+    return numArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
